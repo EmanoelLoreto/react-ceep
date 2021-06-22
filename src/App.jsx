b5213837202667e50e1c48f31dc684714fe1bab8
Emanoel Loreto <emanoel.loreto@meta.com.br>
2021-06-22 18:24:35 -0300

    Revert "Revert "Feature - Delete""
    This reverts commit 52670a87b16df2e6bf2dbe252e5b9ea68a4ff914.

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ export default class App extends Component {
   constructor() {
     super();
     this.cards = [];
+    this.categories = ['Home', 'Work', 'School']
     this.state = {};
   }
 
-  createNote(title, content) {
-    this.cards.push({
-      title: title,
-      content: content,
-    })
-    this.setState({
-      cards: this.cards
-    })
+  createNote(title, content, categorie) {
+    if (
+      title !== ''
+        && content !== ''
+        && categorie !== ''
+    ) {
+      this.cards.push({
+        title: title,
+        content: content,
+        categorie: categorie,
+      })
+      this.setState({
+        cards: this.cards
+      })
+    } else {
+      alert('Preencha todos os campos!');
+    }
   }
 
   deleteNote(index) {
@@ -30,7 +40,12 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <FormNotes createNote={(title, content) => this.createNote(title, content)} />
+        <FormNotes
+          createNote={(title, content, categorie) => {
+            this.createNote(title, content, categorie)
+          }}
+          categories={this.categories}
+        />
         <hr />
         <ListOfNotes cards={this.cards} deleteNote={(event) => this.deleteNote(event)} />
       </>
